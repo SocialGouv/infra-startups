ae74e56b43871ce23048100dcf8dfe43ce36ce61
devthejo <jo@surikat.pro>
2022-12-25 21:21:40 +0100

    refactor: logger + indentation

--- middlewares/fork-logger.js
+++ middlewares/fork-logger.js
@@ -1,16 +1,29 @@
 const { ctx } = require("foundernetes")
 
 module.exports = () => ({
-  hook: () => {
+  hook: (mixed, pluginType) => {
     const loader = ctx.get("loader")
     const play = ctx.get("play")
     const playbook = ctx.get("playbook")
+
+    ctx.replace("indentation", (indent = -1) => ++indent)
+    const indentation = ctx.get("indentation")
+
+    let index
+    if (pluginType === "iteration") {
+      index = mixed.index
+    }
+
     ctx.replace("logger", (log) =>
-      log.child({
-        loader: loader?.name,
-        playbook: playbook?.name,
-        play: play?.name,
-      })
+      log.child(
+        {
+          loader: loader?.name,
+          playbook: playbook?.name,
+          play: play?.name,
+          index,
+        },
+        { indentation: indentation * 2 }
+      )
     )
   },
 })
--- middlewares/iterator-log.js
+++ middlewares/iterator-log.js
@@ -1,16 +1,16 @@
 const { ctx } = require("foundernetes")
 
-// const key = Symbol("async-loop-log")
-module.exports = () => {
-  const logger = ctx.require("logger")
-  return {
-    // key,
-    collection: (collection) => {
+// const key = Symbol("iterator-log")
+module.exports = () => ({
+  // key,
+  hook: ({ collection, item, index }, type) => {
+    const logger = ctx.require("logger")
+    if (type === "collection") {
       logger.info({ collection }, "collection")
-    },
-    iteration: (item, index) => {
+    }
+    if (type === "iteration") {
       logger.info({ item, index }, "iteration")
-    },
-  }
-}
+    }
+  },
+})
 // module.exports.key = key
